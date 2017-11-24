c203ea6b0e8a2337d3f7acdfb4609e5372066d8b
Mwai Karimi <mwaikarimi@Mwais-MacBook-Pro.local>
2017-11-24 17:03:15 +0000

    update URLconf

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import RedirectView
 from blog.sitemaps import PostSiteMap
 
 sitemaps = {
@@ -23,6 +24,7 @@ sitemaps = {
 }
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='/blog/')),
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemaps'),
 ]
