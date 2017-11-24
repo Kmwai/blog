76da0070cff9fa183bec9773e65b422dca7f6d7b
Mwai Karimi <mwaikarimi@Mwais-MacBook-Pro.local>
2017-11-24 17:02:48 +0000

    add tests

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,16 +1,47 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 from blog.models import Post, Comment
 
 
 class PostModelTest(TestCase):
 
+    def setUp(self):
+        self.year = '2017'
+        self.month = '06'
+        self.day = ''
+
+
     def test_str_representation(self):
         """test that a blog post title's string representation is equal to it's title"""
         post = Post(title='My post title')
         self.assertEqual(str(post), post.title)
 
-    def test_get_absolute_url(self):
-        post = Post.published.all(id=1)
-        self.assertEqual(post.get_absolute_url(), '/blog/post/1')
+   # def test_get_absolute_url(self):
+    #    self.assertEqual('/blog/post_detail/', self.test_get_absolute_url())
+
+
+class TestModelMeta(TestCase):
+
+    def test_post_model_data(self):
+        self.assertEqual(('-publish',), Post()._meta.ordering)
+
+    def test_comment_model_data(self):
+        self.assertEqual(('created',), Comment()._meta.ordering)
+
+
+class TestModelFields(TestCase):
+
+    pass
+
+
+class TestModelValidation(TestCase):
+
+    def test_model_validation(self):
+
+
+
+
+
+
 
