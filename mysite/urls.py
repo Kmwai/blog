b2064d00b243ffe01793329b347bbb31c343c7d6
Mwai <mwai@Misfit.home>
2018-04-12 16:17:18 +0100

    minor fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,18 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import re_path, include
+from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.views.generic import RedirectView
 from blog.sitemaps import PostSiteMap
+
+
+# customise admin template
+admin.site.site_header = 'Good News Admin'
+admin.site.site_title = 'Good news Admin Portal'
+admin.site.index_title = 'Welcome to the Good News Blog Portal'
+
 
 sitemaps = {
     'posts': PostSiteMap,
 }
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^$', RedirectView.as_view(url='/blog/')),
-    re_path(r'^blog/', include('blog.urls', namespace='blog')),
-    re_path(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemaps'),
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/blog/')),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap\.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemaps'),
 ]
